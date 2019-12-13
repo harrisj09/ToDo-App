e01e9efaad840ce077eb786f5e797c347cdce2c7
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-13 15:12:45 -0500

    Updated js

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Organize your life
 - JavaScript
 - SCSS
 - BEM Naming
+- Object Oriented Programming
+- MVC Design Pattern
 
 ## Credit for [Design](https://dribbble.com/shots/6570568-Todo-List-App)
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,14 @@
+/*
+
+    index.js is gonna handle the DOM and when the user presses the plus(add a task) and delete button
+    - When pressed create an object from the constructor to create a task to be displayed
+    - Will also be added to an array that will be
+ */
+
 //Date object
 let d = new Date();
 
-class i {
-    constructor() {
+//grabs array from createTask and performs DOM to display array
+class index {
 
-    }
 }
