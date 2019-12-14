8a0f107b7ddb68223984a9f5096dd1c2415a22ca
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-13 20:40:20 -0500

    Updated form

--- assets/scripts/todoModel.js
+++ assets/scripts/todoModel.js
@@ -1,4 +1,5 @@
 /*
 cares about data. In client-side JavaScript, this means Ajax. One advantage of the MVC pattern is you now have a single place for server-side Ajax calls.
-This makes it inviting for fellow programmers who are not familiar with the solution. The model in this design pattern cares only about JSON or objects that come from the server.
+This makes it inviting for fellow programmers who are not familiar with the solution. The model in this design pattern cares only about JSON or objects 
+that come from the server.
  */
