20ec107c494d19e80ddf9e918c5f6c24b0751e91
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-14 15:31:18 -0500

    Fixed todo element

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,3 +5,13 @@ import TodoController from './TodoController.js';
 const todoController = new TodoController(new TodoModel, new TodoView);
 
 todoController.init();
+
+/*
+CSS that will be incremented into DOM
+<div class="task__element">
+            <div class="task__element--complete"> </div>
+            <p class="task__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            <div class="task__element--delete">
+            </div>
+
+ */
