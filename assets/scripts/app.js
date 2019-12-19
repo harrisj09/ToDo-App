74b4566ffe379dabba2e15b9ef96f43a6d20e982
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-19 17:59:41 -0500

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,81 +1,8 @@
-/*
-//console.log() shortcut
-const { log: $, warn: $w, error: $e } = console;
-const btn = document.querySelector(".app__form--submit");
-const tasksSection = document.querySelector(".tasks__wrapper");
-const inputString = document.querySelector(".app__form--submit").value;
-const todoArray = [];
-let pressed = 0;
-let html = ``;
-let number = 0;
-
-btn.addEventListener(`click`, () => {
-  inputString.value = ``;  
-  titleArray.push(inputString);
-  $(`${titleArray[pressed]} was created`);
-  html = ``;
-  number = 0;
-  titleArray.forEach(htmlRewrite);
-  $(html);
-  app2.innerHTML = html;
-  pressed++;
-});
-
-app2.addEventListener("click", event => {
-  const {
-    classList,
-    dataset: { text: value }
-  } = event.target;
-  //grabbing text
-  const holder = event.target;
-  const clickedText = holder.innerHTML;
-  if (classList.contains("title")) {
-    $(`${titleArray.indexOf(clickedText)} array index of clicked text`);
-    //use else if
-  } else {
-    const clickedIndex = event.target.dataset.holder;
-    //Both Work
-    $(
-      `${
-        holder.attributes.getNamedItem("data-holder").value
-      } data attribute of button`
-    );
-    $(event.target.dataset.holder);
-    resizeArray(titleArray, clickedIndex);
-  }
-});
-
-function htmlRewrite(index) {
-  $(number);
-  html += `
- <div>
-    <div class="task__holder" data-task="${index}">
-        <div class="task__holder--complete">Completed</div>
-            <p class="task__desc">${todoArray} ${index}</p>
-            <div class="task__holder--complete task__holder--delete">Delete</div>
-        </div>
-    </div>
-  `;
-  index++;
-  number++;
-}
-
-function resizeArray(array, index) {
-  array.splice(index, 1);
-  $(array);
-  //calling dom
-  html = ``;
-  number = 0;
-  titleArray.forEach(htmlRewrite);
-  $(html);
-  app2.innerHTML = html;
-}
-*/
-
 const { log: $, warn: $w, error: $e } = console;
 const btn = document.querySelector(".app__form--submit");
 const tasksSection = document.querySelector(".tasks__wrapper");
 const todoArray = [];
+const completeArray = [];
 let activeTasks = document.querySelector(".app__tasks-active");
 let pressed = 0;
 let html = ``;
@@ -108,21 +35,18 @@ btn.addEventListener(`click`, () => {
       const holder = event.target;
       const clickedText = holder.innerHTML;
       if (classList.contains("task__holder--complete")) {
-        $(`${todoArray.indexOf(clickedText)}Completed`);
-        //use else if
+        $(`Completed`);
+        //delete this for now but change it be sent to a different function and then delete it in setArraySize
+        setArraySize(todoArray, clickedIndex);
       } 
       else if(classList.contains("task__holder--delete")) {
-        const clickedIndex = event.target.dataset.holder;
-        //Both Work
-        $(
-          `${holder.attributes.getNamedItem("data-delete").value}
-           data attribute of delete`
-        );
-        $(event.target.dataset.holder);
+        const clickedIndex = event.target.dataset.delete;
+        $(event.target.dataset.delete);
         setArraySize(todoArray, clickedIndex);
       }
   });
 
+  //incomplete list
   function htmlRewrite() {
     html += `
     <div>
@@ -134,6 +58,10 @@ btn.addEventListener(`click`, () => {
       </div>
     `;
     counter++;
+  }
+
+  function getCompletedTasks() {
+
   }
 
   function setArraySize(array, index) {
