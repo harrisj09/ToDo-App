e8874507751ba58640097d244191add6c02c6015
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-31 00:58:26 -0500

    Updated MVC

--- assets/scripts/todoController.js
+++ assets/scripts/todoController.js
@@ -6,35 +6,53 @@ class TodoController {
   }
 
   init() {
-    //Basically starts the whole process
-    //Call display date
+    // Basically starts the whole process
+    // Call display date
     this.updateList();
     this.view.displayDate();
     this.displayActiveTasks;
   }
 
-  addTask() {
-    //Call model, then updatelist
+  addTask(text) {
+    this.model.addTodoList(text);
   }
 
   removeTask() {
-    //Call model, then updatelist
+    // Call model, then updatelist
+    // might need a function to handle the logic in grabbing the data attribute
   }
 
   // Changing displays
   completeTask() {
-  //Call model, then updatelist
+    // Call model, then updatelist
   }
 
   // Updates displays of complete or incomplete tasks
   updateList() {
-    //Use foreach loop using a boolean value
+    // Use foreach loop using a boolean value
   }
 
   getActiveTasks() {
     this.view.displayActiveTasks(this.model.getTodoList());
   }
 
+  tasksFilter(type) {
+    let index = 0;
+    let html = ``;
+      for(index in array) {
+      if(array.state[index] == type) {
+        html += `
+        <div>
+          <div class="task__holder">
+            <div class="task__holder--undo" data-undo="${index}">Undo</div>
+                 <p class="task__desc">${this.model.task[index]}</p>
+                 <div class="task__holder--delete" data-delete="${index}">Delete</div>
+             </div>
+           </div>
+        `; 
+      }
+    }
+  }
 }
 
 export default TodoController;
