1781bba8f53efe2db1f0ff0f138689119d739f3f
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-31 08:32:53 -0500

    Update todoController.js

--- assets/scripts/todoController.js
+++ assets/scripts/todoController.js
@@ -15,9 +15,10 @@ class TodoController {
         this.view.errorElement = "Error Input Isn't Long Enough!";
       }
     });
-    // False means incomplete, true is opposite
+    // false means incomplete, true is opposite
     this.view.displayIncompleteTasks.addEventListener("click", tasksFilter(false));
     this.view.displayCompleteTasks.addEventListener("click", taskFilter(true));
+    this.view.tasksSection.addEventListener("click", userClickedOn(event));
     // Functions wil be needed to grab the array index of deleted or completed elements
   }
 
@@ -30,8 +31,29 @@ class TodoController {
   }
 
   // Handles with grabbing what you clicked on by data attribute. 
-  indexOfClickedAction(index, type) {
-
+  userClickedOn(event) {
+    const {
+      classList,
+      dataset: { text: value }
+    } = event.target;
+    //grabbing text
+    const holder = event.target;
+    const clickedText = holder.innerHTML;
+    if (classList.contains("task__holder--complete")) {
+      const clickedIndex = event.target.dataset.complete;
+      // Fix
+      setCompletedTasks(this.model.todos, clickedIndex);
+    } 
+    else if(classList.contains("task__holder--delete")) {
+      const clickedIndex = event.target.dataset.delete;
+      // Fix
+      setArraySize(todoArray, clickedIndex);
+    }
+    else if(classList.contains("task__holder--undo")) {
+      const clickedIndex = event.target.dataset.undo;
+      // Fix
+      //remove it from index in completed array and move it to todoArray
+    }
   }
 
   addTask(text) {
@@ -61,8 +83,10 @@ class TodoController {
   tasksFilter(type) {
     let index = 0;
     let html = ``;
+    if(!type) {
       for(index in array) {
-      if(array.state[index] == type) {
+      if(this.model.todos.state[index] == type) {
+        // should be rewritten for undo
         html += `
         <div>
           <div class="task__holder">
@@ -74,6 +98,23 @@ class TodoController {
         `; 
       }
     }
+  }
+  else {
+    for(index in array) {
+      if(this.model.todos.state[index] == type) {
+        // should be rewritten for undo
+        html += `
+        <div>
+          <div class="task__holder">
+            <div class="task__holder--undo" data-undo="${index}">Undo</div>
+              <p class="task__desc">${this.model.task[index]}</p>
+              <div class="task__holder--delete" data-undo="${index}">Delete</div>
+             </div>
+           </div>
+        `; 
+      }
+    }
+  }
     this.view.displayTasks(html);
   }
 }
